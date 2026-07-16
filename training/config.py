4e6679e8b8ada77decb518fdba6149cdafffabe7
Navid Samanghoon <n.jsamanghoon@gmail.com>
2026-07-16 16:21:00 +0200

    Shared configuration

---
+++ training/config.py
@@ -0,0 +1,25 @@
+"""
+Module used to ensure identical training configuration across all models. By 
+standardizing training, the effect of architectural decisions is isolated, and
+can fairly be measured.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    """Training configuration shared across all experiments."""
+    # Optimization
+    epochs: int = 10
+    lr: float = 1e-3
+    batch_size: int = 64
+
+    # Dataloader
+    shuffle: bool = True
+    num_workers: int = 2
+
+    # Reproducability
+    seed: int = 23
+
+CFG = Config()
