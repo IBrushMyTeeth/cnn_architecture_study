ad3f74371a66332d4e25eb81bd4cd6e9882b67ff
Navid Samanghoon <n.jsamanghoon@gmail.com>
2026-07-20 20:31:48 +0200

    Small adjustments

--- data/transforms.py
+++ data/transforms.py
@@ -14,7 +14,6 @@ from torchvision.transforms import (
     Normalize,
     RandomCrop,
     RandomHorizontalFlip,
-    RandomVerticalFlip,
     ColorJitter,
 )
 
@@ -30,26 +29,30 @@ basic_transform = Compose([
 # Horizontal and vertical flipping only.
 flip_transform = Compose([
     RandomHorizontalFlip(p=0.5),
-    RandomVerticalFlip(p=0.5),
-    ToTensor()
+    ToTensor(),
+    Normalize(mean=MEAN, std=STD)
 ])
 
 # Brightness and contrast augmentation only.
 color_transform = Compose([
     ColorJitter(brightness=0.2, contrast=0.2),
-    ToTensor()
+    ToTensor(),
+    Normalize(mean=MEAN, std=STD)
 ])
 
 # Random cropping only.
 crop_transform = Compose([
-    RandomCrop(size=32, padding=4), ToTensor()
+    RandomCrop(size=32, padding=4),
+    ToTensor(),
+    Normalize(mean=MEAN, std=STD)
 ])
 
 # Full augmentation pipeline used during training.
 train_transform = Compose([
     RandomCrop(size=32, padding=4),
-    RandomHorizontalFlip(p=0.2),
-    RandomVerticalFlip(0.2),
+    # Removed vertical flip since an upside deer, horse, cat, etc. dont make sense
+    # RandomVerticalFlip(0.5),
+    RandomHorizontalFlip(p=0.5),
     ColorJitter(brightness=0.2, contrast=0.2),
     ToTensor(),
     Normalize(mean=MEAN, std=STD)
